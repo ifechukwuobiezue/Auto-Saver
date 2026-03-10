9d808d4c898faf222db568ca2e33687e09ee4ca9
ifechukwuobiezue <ifechukwuobiezuedoc@gmail.com>
2026-03-10 22:36:37 +0100

    pup flags

--- terminal/index.js
+++ terminal/index.js
@@ -89,7 +89,12 @@ async function saveContact(name, phone) {
 }
 
 // ===== WHATSAPP CLIENT =====
-const client = new Client({ authStrategy: new LocalAuth() });
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+  }
+});
 
 client.on("qr", (qr) => qrcode.generate(qr, { small: true }));
 
