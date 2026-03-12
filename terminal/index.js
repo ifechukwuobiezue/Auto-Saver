128c4a0d0723fb54ea96b1b0b27ccd5130fa6b46
ifechukwuobiezue <you@example.com>
2026-03-12 04:22:45 +0100

    dockerify4

--- terminal/index.js
+++ terminal/index.js
@@ -98,7 +98,7 @@ async function start() {
     authStrategy: new LocalAuth(),
     puppeteer: {
       headless: true,
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium",
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
